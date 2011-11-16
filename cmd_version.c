d3a71299693776a0cdecdc76b36e608ab7d550d8
LEVAI Daniel <leva@ecentrum.hu>
2011-11-16 13:22:10 +0000

    add some information about the application and myself

--- cmd_version.c
+++ cmd_version.c
@@ -31,4 +31,7 @@ void
 cmd_version(const char *e_line, command *commands)
 {
 	printf("%s\n", VERSION);
+	puts("kc was written by Daniel LEVAI <leva@ecentrum.hu>");
+	puts("Source, information, bugs:");
+	puts("http://keychain.googlecode.com");
 } /* cmd_version() */
