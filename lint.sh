37551add50bed6c692b1c59dcec921f0eae1ba8e
LEVAI Daniel <leva@ecentrum.hu>
2013-01-13 19:00:23 +0000

    No need for this anymore.

--- lint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-set -e
-
-
-lint -i -hx $(pkg-config --cflags libxml-2.0) $(pkg-config --libs libxml-2.0) \
-	$(pkg-config --cflags libpcre) $(pkg-config --libs libpcre) \
-	-lssl -lcrypto -ledit -lncursesw \
-	-D_HAVE_PCRE $@
