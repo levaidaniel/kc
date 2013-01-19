1f213cbe93d30c8dffbbd337cf2cc3429cbd2f78
LEVAI Daniel <leva@ecentrum.hu>
2013-01-19 15:35:26 +0000

    Adjust export test to recent changes (encrypted output and new 'dump' command).

--- regress/cmd_export.sh
+++ regress/cmd_export.sh
@@ -18,20 +18,39 @@ case "$(uname -s)" in
 	;;
 esac
 
-rm -f regress/test_export
+rm -f regress/test_export.kcd
 
-printf "xport regress/test_export\n" |./kc -b -k regress/test -p regress/testpass
+PASSWORD=aabbccdd112233
 
-if [ ! -r "regress/test_export" ];then
+printf "xport regress/test_export\n${PASSWORD}\n${PASSWORD}\n" |./kc -b -k regress/test -p regress/testpass
+
+if [ ! -r "regress/test_export.kcd" ];then
 	echo "$0 test failed (unreadable export file)!"
 	exit 1
 fi
 
-SHA1=$(cat regress/test_export |sed -e 's/created="[0-9]\{1,\}"//' -e 's/modified="[0-9]\{1,\}"//' |$SHA1_BIN |cut -d' ' -f1)
-if [ "$SHA1" = '23fa5cccf87201c76a9fd7b29be13cfda3b2295b' ];then
-	echo $0 test ok!
+if printf "${PASSWORD}" |./kc -b -k regress/test_export.kcd;then
+	echo "$0 test ok (xport)!"
 else
-	echo $0 test failed!
+	echo "$0 test failed (xport)!"
+	exit 1
+fi
+
+
+rm -f regress/test_dump.xml
+
+printf "dump regress/test_dump\n" |./kc -b -k regress/test -p regress/testpass
+
+if [ ! -r "regress/test_dump.xml" ];then
+	echo "$0 test failed (unreadable dump file)!"
+	exit 1
+fi
+
+SHA1=$(cat regress/test_dump.xml |sed -e 's/created="[0-9]\{1,\}"//' -e 's/modified="[0-9]\{1,\}"//' |$SHA1_BIN |cut -d' ' -f1)
+if [ "$SHA1" = '23fa5cccf87201c76a9fd7b29be13cfda3b2295b' ];then
+	echo "$0 test ok (dump)!"
+else
+	echo "$0 test failed (dump)!"
 	exit 1
 fi
 
