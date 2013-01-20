ddb5463629c2a4f53dd3a5751b85ac056d8b9e99
LEVAI Daniel <leva@ecentrum.hu>
2013-01-20 16:50:30 +0000

    Fix previous...

--- cmd_write.c
+++ cmd_write.c
@@ -110,7 +110,7 @@ cmd_write(const char *e_line, command *commands)
 	}
 
 	stat(db_filename_tmp, &st);
-	if (st->st_size <= (IV_LEN + SALT_LEN + 1)) {	/* "+ 1" is for a newline */
+	if (st.st_size <= (IV_LEN + SALT_LEN + 1)) {	/* "+ 1" is for a newline */
 		puts("Temporary database file became unusally small!");
 
 		BIO_free_all(bio_chain_tmp);
