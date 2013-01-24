8098705fedfd8668472ffcece7682038bb9830d1
LEVAI Daniel <leva@ecentrum.hu>
2013-01-24 10:35:43 +0000

    Make version test more comfortable.

--- regress/cmd_version.sh
+++ regress/cmd_version.sh
@@ -6,10 +6,8 @@ set -e
 echo "test => $0"
 
 NAME=$(grep -E -e '^#define[[:space:]]+NAME' common.h |cut -f3 |tr -d '"')
-VERSION=$(grep -E -e '^#define[[:space:]]+VERSION' common.h |cut -f3 |tr -d '"')
-SVN_VERSION=$(svn info |awk '/^Revision:/ {print $2}')
 
-if printf "version\n" |./kc -b -k regress/test -p regress/testpass |grep -E -e "^${NAME} $(echo ${VERSION} |cut -d- -f1)-$(echo ${VERSION} |cut -d- -f2)-(${SVN_VERSION}|SVN_VERSION)";then
+if printf "version\n" |./kc -b -k regress/test -p regress/testpass |grep -E -e "^${NAME} [[:alnum:]\.\-]+";then
 	echo "$0 test ok (version)!"
 else
 	echo "$0 test failed (version)!"
