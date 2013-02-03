cdd77e091851b730da0c0207aa857186d7bb7c20
LEVAI Daniel <leva@ecentrum.hu>
2013-02-03 13:43:26 +0000

    Adjust tests to previous change to version information (regarding PCRE support).

--- regress/stress_test.sh
+++ regress/stress_test.sh
@@ -39,7 +39,7 @@ case "$(uname -s)" in
 	;;
 esac
 
-if ./kc -v |grep -E -q -e '^Compiled with Readline(, RegExp)* support\.$';then
+if ./kc -v |grep -E -q -e '^Compiled with Readline(, PCRE)* support\.$';then
 	export READLINE=readline
 fi
 
