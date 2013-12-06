be77f6f368efc1e19dd3597671343dc54e4f8bf3
LEVAI Daniel <leva@ecentrum.hu>
2013-12-06 19:31:37 +0100

    Restore the original password after the password change completed.

--- regress/cmd_passwd.sh
+++ regress/cmd_passwd.sh
@@ -5,6 +5,7 @@ set -e
 
 echo "test => $0"
 
+OLDPASSWORD=$(< ${KC_PASSFILE})
 PASSWORD='123abc321ABC'
 
 if printf "passwd\n${PASSWORD}\n${PASSWORD}\n" |./kc -b -k ${KC_DB} -p ${KC_PASSFILE};then
@@ -15,6 +16,15 @@ else
 fi
 
 echo "${PASSWORD}" > ${KC_PASSFILE}
+
+if printf "passwd\n${OLDPASSWORD}\n${OLDPASSWORD}\n" |./kc -b -k ${KC_DB} -p ${KC_PASSFILE};then
+	echo "$0 test ok (passwd)!"
+else
+	echo "$0 test failed (passwd)!"
+	exit 1
+fi
+
+echo "${OLDPASSWORD}" > ${KC_PASSFILE}
 
 if echo "" |./kc -b -k ${KC_DB} -p ${KC_PASSFILE};then
 	echo "$0 test ok (reopen)!"
