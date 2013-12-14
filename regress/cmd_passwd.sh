289b93272b1916f46b06b21c5ed60c057d80b3c6
LEVAI Daniel <leva@ecentrum.hu>
2013-12-14 18:23:14 +0100

    Regression test for 'passwd' commands KDF changer (bcrypt)

--- regress/cmd_passwd.sh
+++ regress/cmd_passwd.sh
@@ -5,26 +5,40 @@ set -e
 
 echo "test => $0"
 
-OLDPASSWORD=$(< ${KC_PASSFILE})
-PASSWORD='123abc321ABC'
+PASSWORD=$(< ${KC_PASSFILE})
+NEWPASSWORD='123abc321ABC'
 
-if printf "passwd\n${PASSWORD}\n${PASSWORD}\n" |./kc -b -k ${KC_DB} -p ${KC_PASSFILE};then
+if printf "passwd\n${NEWPASSWORD}\n${NEWPASSWORD}\n" |./kc -b -k ${KC_DB} -p ${KC_PASSFILE};then
 	echo "$0 test ok (passwd)!"
 else
 	echo "$0 test failed (passwd)!"
 	exit 1
 fi
 
-echo "${PASSWORD}" > ${KC_PASSFILE}
+echo "${NEWPASSWORD}" > ${KC_PASSFILE}
 
-if printf "passwd\n${OLDPASSWORD}\n${OLDPASSWORD}\n" |./kc -b -k ${KC_DB} -p ${KC_PASSFILE};then
-	echo "$0 test ok (old passwd)!"
+if printf "passwd\n${PASSWORD}\n${PASSWORD}\n" |./kc -b -k ${KC_DB} -p ${KC_PASSFILE};then
+	echo "$0 test ok (orig passwd)!"
 else
-	echo "$0 test failed (old passwd)!"
+	echo "$0 test failed (orig passwd)!"
 	exit 1
 fi
 
-echo "${OLDPASSWORD}" > ${KC_PASSFILE}
+echo "${PASSWORD}" > ${KC_PASSFILE}
+
+if printf "passwd bcrypt\n${PASSWORD}\n${PASSWORD}\n" |./kc -b -k ${KC_DB} -p ${KC_PASSFILE};then
+	echo "$0 test ok (passwd bcrypt #1)!"
+else
+	echo "$0 test failed (passwd bcrypt #1)!"
+	exit 1
+fi
+
+if printf "passwd sha512\n${PASSWORD}\n${PASSWORD}\n" |./kc -b -k ${KC_DB} -P bcrypt -p ${KC_PASSFILE};then
+	echo "$0 test ok (passwd bcrypt #2)!"
+else
+	echo "$0 test failed (passwd bcrypt #2)!"
+	exit 1
+fi
 
 if echo "" |./kc -b -k ${KC_DB} -p ${KC_PASSFILE};then
 	echo "$0 test ok (reopen)!"
