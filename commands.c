b0dc0b2888ecfbedfeec1fe6661239d74f151676
LEVAI Daniel <leva@ecentrum.hu>
2014-01-01 02:49:33 +0100

    Let's get this over with: copyright year update.

--- commands.c
+++ commands.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2013 LEVAI Daniel
+ * Copyright (c) 2011-2014 LEVAI Daniel
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
