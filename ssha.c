d55a5f4c90d7762d660116c0b5ea5f6ca1c320bc
LEVAI Daniel <leva@ecentrum.hu>
2018-07-29 22:51:13 +0200

    ssha.c: consolidate error/debug logging and checks

--- ssha.c
+++ ssha.c
@@ -48,24 +48,26 @@ kc_ssha_connect(void)
 	addr.sun_family = AF_UNIX;
 
 	sock_name = getenv("SSH_AUTH_SOCK");
+	if (getenv("KC_DEBUG"))
+		printf("OpenSSH agent UNIX socket: '%s'\n", sock_name);
 	if (sock_name == NULL  ||  strlen(sock_name) <= 0) {
-		dprintf(STDERR_FILENO, "sock_name is NULL or of zero length\n");
+		dprintf(STDERR_FILENO, "OpenSSH agent socket name is NULL or zero length\n");
 		return(-1);
 	}
 
 	if (strlcpy(addr.sun_path, sock_name, sizeof(addr.sun_path)) >= sizeof(addr.sun_path)) {
-		dprintf(STDERR_FILENO, "truncated strlcpy() when copying sock_name to sun_path\n");
+		dprintf(STDERR_FILENO, "Truncated strlcpy() when copying sock_name to sun_path\n");
 		return(-1);
 	}
 
 	sock = socket(AF_UNIX, SOCK_STREAM, 0);
 	if (sock <= 0) {
-		dprintf(STDERR_FILENO, "couldn't create UNIX socket\n");
+		dprintf(STDERR_FILENO, "Could not create UNIX socket for OpenSSH agent communication\n");
 		return(-1);
 	}
 
 	if (connect(sock, (struct sockaddr*)&addr, sizeof(struct sockaddr_un)) != 0) {
-		dprintf(STDERR_FILENO, "couldn't connect to UNIX socket: '%s'\n", sock_name);
+		dprintf(STDERR_FILENO, "Could not connect to OpenSSH agent UNIX socket: '%s'\n", sock_name);
 		return(-1);
 	}
 
@@ -81,18 +83,13 @@ kc_ssha_get_identity_list(int sock)
 	size_t	w = 0, buf_len = 1024;
 
 
-	buf = calloc(1, buf_len);
-	if (buf == NULL) {
-		/* TODO Error message */
-		return(-1);
-	}
+	buf = calloc(1, buf_len); malloc_check(buf);
 
 	/* my request's length */
 	put_u32(buf, sizeof(request));
 	w = write(sock, buf, 4);
-	dprintf(STDERR_FILENO, "written = %zd\n", w);
 	if (w != 4 ) {
-		perror("write() error");
+		perror("socket write() error");
 		free(buf); buf = NULL;
 		return(-1);
 	} else {
@@ -103,9 +100,8 @@ kc_ssha_get_identity_list(int sock)
 	/* my request */
 	memcpy(buf, &request, 1);
 	w = write(sock, buf, 1);
-	dprintf(STDERR_FILENO, "written = %zd\n", w);
 	if (w != 1 ) {
-		perror("write() error");
+		perror("socket write() error");
 		free(buf); buf = NULL;
 		return(-1);
 	}
@@ -126,7 +122,7 @@ kc_ssha_sign_request(int sock, struct kc_ssha_identity *id, char *data, unsigned
 
 
 	if (data == NULL) {
-		dprintf(STDERR_FILENO, "data to sign is NULL\n");
+		dprintf(STDERR_FILENO, "Data to sign is empty\n");
 		return(-1);
 	}
 
@@ -136,11 +132,7 @@ kc_ssha_sign_request(int sock, struct kc_ssha_identity *id, char *data, unsigned
 	req_len = 5 + 4 + id->pubkey_len + 4 + strlen(data);
 	buf_len = req_len + 4;	/* plus flags field */
 
-	buf = calloc(1, buf_len);
-	if (buf == NULL) {
-		/* TODO Error message */
-		return(-1);
-	}
+	buf = calloc(1, buf_len); malloc_check(buf);
 
 	/* my request's length */
 	put_u32(len, req_len);
@@ -179,14 +171,17 @@ kc_ssha_sign_request(int sock, struct kc_ssha_identity *id, char *data, unsigned
 	memcpy(buf+pos, len, 4);
 	pos += 4;
 
-	dprintf(STDERR_FILENO, "buf_len was %zd and we've copied %zd\n", buf_len, pos);
+	if (buf_len != pos) {
+		dprintf(STDERR_FILENO, "Total copied bytes is different from the buffer size\n");
+		free(buf); buf = NULL;
+		return(-1);
+	}
 
 	pos = 0;
 	do {
 		w = write(sock, buf+pos, buf_len-pos);
-		dprintf(STDERR_FILENO, "wrote = %zd\n", w);
 		if (w <= 0) {
-			perror("write() error");
+			perror("socket write() error");
 			free(buf); buf = NULL;
 			return(-1);
 		}
@@ -195,7 +190,8 @@ kc_ssha_sign_request(int sock, struct kc_ssha_identity *id, char *data, unsigned
 		wt += w;
 	} while (pos < buf_len);
 
-	dprintf(STDERR_FILENO, "wrote total = %zd\n", wt);
+	if (getenv("KC_DEBUG"))
+		dprintf(STDERR_FILENO, "wrote total = %zd\n", wt);
 
 
 	free(buf); buf = NULL;
@@ -216,24 +212,22 @@ kc_ssha_get_full_response(int sock)
 
 
 	buf_len = 10; buf_rem = buf_len;	/* XXX DEBUG */
-	buf = malloc(buf_len);
-	if (buf == NULL) {
-		/* TODO Error message */
-		return(NULL);
-	}
+	buf = malloc(buf_len); malloc_check(buf);
 
 	/* first read the response's length */
 	r = read(sock, buf, 4);
-	dprintf(STDERR_FILENO, "read = %zd\n", r);
-	if (r <= 0 ) {
-		perror("read() error");
+	if (r != 4 ) {
+		perror("socket read() error");
 		free(buf); buf = NULL;
 		return(NULL);
 	};
+
 	len = get_u32(buf);
-	dprintf(STDERR_FILENO, "len = %zd\n", len);
+	if (getenv("KC_DEBUG"))
+		dprintf(STDERR_FILENO, "response length = %zd\n", len);
+
 	if (len > MAX_AGENT_REPLY_LEN) {
-		dprintf(STDERR_FILENO, "len(%zd) is more than MAX_AGENT_REPLY_LEN(%d)\n", len, MAX_AGENT_REPLY_LEN);
+		dprintf(STDERR_FILENO, "Indicated response length(%zd) is more than MAX_AGENT_REPLY_LEN(%d)\n", len, MAX_AGENT_REPLY_LEN);
 		free(buf); buf = NULL;
 		return(NULL);
 	} else {
@@ -242,66 +236,51 @@ kc_ssha_get_full_response(int sock)
 	}
 
 	rem = len;
-	dprintf(STDERR_FILENO, "rem = %zd\n", rem);
-	dprintf(STDERR_FILENO, "buf_len = %zd\n", buf_len);
 	while (rem  &&  buf_rem  &&  buf_len < MAX_AGENT_REPLY_LEN) {
 		if (rem > buf_rem) {
 			buf_len_prev = buf_len;
 			buf_len = ++buf_len_mult * buf_len_base;
-			dprintf(STDERR_FILENO, "realloc buf to %zd\n", buf_len);
-			nbuf = realloc(buf, buf_len);
-			if (nbuf == NULL) {
-				/* TODO Error message */
-				free(buf); buf = NULL;
-				return(NULL);
-			}
+			if (getenv("KC_DEBUG"))
+				dprintf(STDERR_FILENO, "realloc response buf to %zd\n", buf_len);
+			nbuf = realloc(buf, buf_len); malloc_check(nbuf);
 
 			buf = nbuf;
 			buf_rem += buf_len - buf_len_prev;
-			dprintf(STDERR_FILENO, "buf_rem = %zd\n", buf_rem);
 
 			continue;
 		}
 
 		r = read(sock, buf+(buf_len - buf_rem), rem);
-		dprintf(STDERR_FILENO, "read = %zd\n", r);
 		switch (r) {
 			case -1:
-				perror("read() error");
+				perror("socket read() error");
 				free(buf); buf = NULL;
 				return(NULL);
 			break;
 			case 0:
 			default:
-				dprintf(STDERR_FILENO, "read 0 (EOF)\n");
 				rem -= r;	/* this is what remained in the socket */
-				dprintf(STDERR_FILENO, "rem = %zd\n", rem);
 				rt += r;	/* this is total bytes read */
-				dprintf(STDERR_FILENO, "rt = %zd\n", rt);
 				buf_rem -= r;	/* this is what my buffer can still hold */
-				dprintf(STDERR_FILENO, "buf_rem = %zd\n", buf_rem);
 			break;
 		};
 	}
 
+	if (getenv("KC_DEBUG"))
+		dprintf(STDERR_FILENO, "read total = %zd\n", rt);
 
-	/* we didn't read as much as we should've */
 	if (rt != len) {
-		/* TODO Error message */
+		dprintf(STDERR_FILENO, "Total read bytes is different from length indication in response\n");
 		free(buf); buf = NULL;
 		return(NULL);
 	}
 
 
 	/* create the response struct */
-	response = malloc(sizeof(struct kc_ssha_response));
-	if (response == NULL) {
-		/* TODO Error message */
-		free(buf);
-		return(NULL);
-	}
+	response = malloc(sizeof(struct kc_ssha_response)); malloc_check(response);
 	response->data = buf;
 	response->length = rt;
+
 
 	return(response);
 } /* kc_ssha_get_full_response() */
@@ -316,8 +295,11 @@ kc_ssha_parse_identities(struct kc_ssha_response *response)
 
 
 	num_ids = get_u32(response->data+pos);
-	dprintf(STDERR_FILENO, "agent has %zd identities\n", num_ids);
+	if (getenv("KC_DEBUG"))
+		dprintf(STDERR_FILENO, "OpenSSH agent has %zd identities\n", num_ids);
+
 	if (num_ids <= 0) {
+		dprintf(STDERR_FILENO, "OpenSSH agent has no identities\n");
 		return(NULL);
 	}
 	pos += 4;
@@ -326,7 +308,7 @@ kc_ssha_parse_identities(struct kc_ssha_response *response)
 		if (idlist != NULL)	/* not first call */
 			idlist = idlist->next;
 		else
-			idlist = calloc(1, sizeof(struct kc_ssha_identity));
+			idlist = calloc(1, sizeof(struct kc_ssha_identity)); malloc_check(idlist);
 
 		if (idlist_first == NULL)	/* first call */
 			idlist_first = idlist;
@@ -334,13 +316,13 @@ kc_ssha_parse_identities(struct kc_ssha_response *response)
 		/* pubkey length */
 		idlist->pubkey_len = get_u32(response->data+pos);
 		if (idlist->pubkey_len > KC_MAX_PUBKEY_LEN) {
-			dprintf(STDERR_FILENO, "pubkey length is larger than allowed\n");
+			dprintf(STDERR_FILENO, "Public key length is larger than allowed\n");
 			return(NULL);
 		}
 		pos += 4;
 
 		/* public key */
-		idlist->pubkey = calloc(1, idlist->pubkey_len);
+		idlist->pubkey = calloc(1, idlist->pubkey_len); malloc_check(idlist->pubkey);
 		memcpy(idlist->pubkey, response->data+pos, idlist->pubkey_len);
 		pos += idlist->pubkey_len;
 
@@ -349,22 +331,22 @@ kc_ssha_parse_identities(struct kc_ssha_response *response)
 		 * which are part of the pubkey blob
 		 */
 		slen = get_u32(idlist->pubkey);
-		idlist->type = calloc(1, slen + 1);
+		idlist->type = calloc(1, slen + 1); malloc_check(idlist->type);
 		memcpy(idlist->type, idlist->pubkey+4, slen); idlist->type[slen] = '\0';
 		/*				^^^^^ + 4 here is the length indication of the key type string */
 
 		/* key comment */
 		slen = get_u32(response->data+pos);
 		if (slen > KC_MAX_PUBKEY_COMMENT_LEN) {
-			dprintf(STDERR_FILENO, "pubkey comment length is larger than allowed\n");
+			dprintf(STDERR_FILENO, "Public key comment length is larger than allowed\n");
 			return(NULL);
 		}
 		pos += 4;
-		idlist->comment = calloc(1, slen);
+		idlist->comment = calloc(1, slen); malloc_check(idlist->comment);
 		memcpy(idlist->comment, response->data+pos, slen);
 		pos += slen;
 
-		idlist->next = calloc(1, sizeof(struct kc_ssha_identity));
+		idlist->next = calloc(1, sizeof(struct kc_ssha_identity)); malloc_check(idlist->next);
 	} while (--num_ids);
 	free(idlist->next); idlist->next = NULL;
 
@@ -383,16 +365,17 @@ kc_ssha_parse_signature(struct kc_ssha_response *response)
 
 	len = get_u32(response->data+pos);
 	pos += 4;
-	dprintf(STDERR_FILENO, "signature length is %zd\n", len);
+	if (getenv("KC_DEBUG"))
+		printf("signature length is %zd\n", len);
 
 	if (len > KC_MAX_SIGNATURE_LEN) {
-		dprintf(STDERR_FILENO, "signature length is larger than allowed\n");
+		dprintf(STDERR_FILENO, "Signature length is larger than allowed\n");
 		return(NULL);
 	}
-	s = calloc(1, len);
+	s = calloc(1, len); malloc_check(s);
 	memcpy(s, response->data+pos, len);
 
-	signature = calloc(1, sizeof(struct kc_ssha_signature));
+	signature = calloc(1, sizeof(struct kc_ssha_signature)); malloc_check(signature);
 	signature->signature = s;
 	signature->length = len;
 
@@ -413,49 +396,37 @@ kc_ssha_get_password(char *type, char *comment)
 	struct kc_ssha_signature 	*signature = NULL;
 
 
-	data_to_sign = malloc(IV_DIGEST_LEN + SALT_DIGEST_LEN + 1); malloc_check(data_to_sign);
-	dprintf(STDERR_FILENO, "Using iv for signing: '%s'\n", db_params.iv);
-	dprintf(STDERR_FILENO, "Using salt for signing: '%s'\n", db_params.salt);
-	if (strlcpy(data_to_sign, (const char*)db_params.iv, IV_DIGEST_LEN + 1) >= IV_DIGEST_LEN + 1) {
-		puts("Error while setting up OpenSSH agent signing.");
-		free(data_to_sign); data_to_sign = NULL;
-		return(NULL);
-	}
-	dprintf(STDERR_FILENO, "data_to_sign: '%s'\n", data_to_sign);
-	if (strlcat(data_to_sign, (const char*)db_params.salt, SALT_DIGEST_LEN + IV_DIGEST_LEN + 1) >= SALT_DIGEST_LEN + IV_DIGEST_LEN + 1) {
-		puts("Error while setting up OpenSSH agent signing.");
-		free(data_to_sign); data_to_sign = NULL;
-		return(NULL);
-	}
-	dprintf(STDERR_FILENO, "data_to_sign: '%s'\n", data_to_sign);
-
 	sock = kc_ssha_connect();
 	if (sock < 0) {
-		dprintf(STDERR_FILENO, "couldn't establish UNIX socket connection\n");
+		dprintf(STDERR_FILENO, "Couldn't establish UNIX socket connection\n");
 		return(NULL);
 	}
 
 	/* get identities */
 	if (kc_ssha_get_identity_list(sock) < 0) {
-		dprintf(STDERR_FILENO, "failed to send identity list request\n");
+		dprintf(STDERR_FILENO, "Failed to send identity list request\n");
+		return(NULL);
 	}
 	response = kc_ssha_get_full_response(sock);
 	if (response == NULL) {
-		/* TODO Error message */
+		dprintf(STDERR_FILENO, "Could not get response for identity list request\n");
 		return(NULL);
 	}
 
 	response_type = (char)*response->data;
-	dprintf(STDERR_FILENO, "response type is %d\n", response_type);
+	if (getenv("KC_DEBUG"))
+		printf("response type is %d\n", response_type);
 	if (agent_failed(response_type)) {
-		printf("auth agent something failed\n");
+		dprintf(STDERR_FILENO, "OpenSSH agent request failed\n");
+		return(NULL);
 	} else if (response_type != SSH2_AGENT_IDENTITIES_ANSWER) {
-		printf("invalid response type\n");
+		dprintf(STDERR_FILENO, "Invalid OpenSSH agent response type\n");
+		return(NULL);
 	}
 
 	idlist = kc_ssha_parse_identities(response);
 	if (idlist == NULL) {
-		/* TODO Error message */
+		dprintf(STDERR_FILENO, "Could not parse identity list\n");
 		return(NULL);
 	}
 
@@ -469,40 +440,56 @@ kc_ssha_get_password(char *type, char *comment)
 	} while (idlist != NULL);
 
 	if (idlist == NULL) {
-		printf("could not find a match for (%s) %s\n", type, comment);
+		dprintf(STDERR_FILENO, "Could not find a match for identity: (%s) %s\n", type, comment);
 		return(NULL);
-	} else {
-		printf("found a match for (%s) %s\n", type, comment);
 	}
 
 
 	/* ask for a signature */
-	if (kc_ssha_sign_request(sock, idlist, data_to_sign, 0) < 0) {
-		dprintf(STDERR_FILENO, "failed to send signature request\n");
+	data_to_sign = malloc(IV_DIGEST_LEN + SALT_DIGEST_LEN + 1); malloc_check(data_to_sign);
+	if (strlcpy(data_to_sign, (const char*)db_params.iv, IV_DIGEST_LEN + 1) >= IV_DIGEST_LEN + 1) {
+		dprintf(STDERR_FILENO, "Error while setting up OpenSSH agent signing.\n");
+		free(data_to_sign); data_to_sign = NULL;
 		return(NULL);
 	}
+	if (strlcat(data_to_sign, (const char*)db_params.salt, SALT_DIGEST_LEN + IV_DIGEST_LEN + 1) >= SALT_DIGEST_LEN + IV_DIGEST_LEN + 1) {
+		dprintf(STDERR_FILENO, "Error while setting up OpenSSH agent signing.\n");
+		free(data_to_sign); data_to_sign = NULL;
+		return(NULL);
+	}
+
+	if (kc_ssha_sign_request(sock, idlist, data_to_sign, 0) < 0) {
+		dprintf(STDERR_FILENO, "Failed to send signature request\n");
+		free(data_to_sign); data_to_sign = NULL;
+		return(NULL);
+	}
+	free(data_to_sign); data_to_sign = NULL;
+
 	response = kc_ssha_get_full_response(sock);
 	if (response == NULL) {
-		/* TODO Error message */
+		dprintf(STDERR_FILENO, "Could not get response for signature request\n");
 		return(NULL);
 	}
 
 	response_type = (char)*response->data;
-	dprintf(STDERR_FILENO, "response type is %d\n", response_type);
+	if (getenv("KC_DEBUG"))
+		printf("response type is %d\n", response_type);
 	if (agent_failed(response_type)) {
-		printf("auth agent something failed\n");
+		dprintf(STDERR_FILENO, "OpenSSH agent request failed\n");
+		return(NULL);
 	} else if (response_type != SSH2_AGENT_SIGN_RESPONSE) {
-		printf("invalid response type\n");
+		dprintf(STDERR_FILENO, "Invalid OpenSSH agent response type\n");
+		return(NULL);
 	}
 
 	signature = kc_ssha_parse_signature(response);
 	if (signature == NULL) {
-		/* TODO Error message */
+		dprintf(STDERR_FILENO, "Could not parse signature response\n");
 		return(NULL);
 	}
 
 	if (signature->length > PASSWORD_MAXLEN) {
-		dprintf(STDERR_FILENO, "PASSWORD_MAXLEN is smaller than the signature length\n");
+		dprintf(STDERR_FILENO, "Signature length is larger than the allowed password length\n");
 		return(NULL);
 	}
 
@@ -513,7 +500,10 @@ kc_ssha_get_password(char *type, char *comment)
 		memcpy(password+pos, &hex, 2);
 		pos += 2;
 	}
-	dprintf(STDERR_FILENO, "password from agent signature is '%s'\n", password);
+	memset(signature->signature, 0, signature->length);
+	free(signature->signature); signature->signature = NULL;
+	free(signature); signature = NULL;
+
 
 	return(password);
 } /* kc_ssha_get_password() */
