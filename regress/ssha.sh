787dfd79b9ccf510109a08c801bffa6962ec039f
LEVAI Daniel <leva@ecentrum.hu>
2018-07-30 14:14:34 +0200

    regress/ssha.sh: test with multiple key types
    Also use the new OpenSSH file format (-o option to ssh-keygen) to store
    the key comment.
    Remove 'status' command test to make this simpler.

--- regress/ssha.sh
+++ regress/ssha.sh
@@ -22,37 +22,38 @@ if [ $? -ne 0 ];then
 	exit 1
 fi
 
-SSH_ID='regress/test_id_ssh'
-SSH_ID_TYPE='ed25519'
-SSH_ID_COMMENT="this is what we're looking for"
-rm -f "${SSH_ID}" "${SSH_ID}".pub
-${SSH_KEYGEN} -t "${SSH_ID_TYPE}" -C "${SSH_ID_COMMENT}" -N '' -f "${SSH_ID}"
-if [ $? -ne 0 ];then
-	echo "$0 test failed (generate new ssh key)!"
-	exit 1
-fi
-
-${SSH_ADD} "${SSH_ID}"
-if [ $? -ne 0 ];then
-	echo "$0 test failed (add new ssh key to agent)!"
-	exit 1
-fi
-
 KC_DB='regress/test_ssha.kcd'
-rm -f "${KC_DB}"
-if echo "write" |${KC_RUN} -A "ssh-${SSH_ID_TYPE},${SSH_ID_COMMENT}" -b -k ${KC_DB};then
-	echo "$0 test ok (create new db with agent)!"
-else
-	echo "$0 test failed (create new db with agent)!"
-	exit 1
-fi
+for SSH_ID_TYPE in ecdsa ed25519 rsa;do
+	SSH_ID_FILE="regress/test_id_${SSH_ID_TYPE}_ssh"
+	SSH_ID_COMMENT="this is what we're looking for"
 
-SHA1=$(echo "status" |${KC_RUN} -A "ssh-${SSH_ID_TYPE},${SSH_ID_COMMENT}" -b -k ${KC_DB} |grep -E -v -e '^<default% >' -e "^Opening '${KC_DB}'" -e "^Using '${KC_DB}' database." -e '^Database file:' |$SHA1_BIN |cut -d' ' -f1)
-if [ "$SHA1" = 'c98403e5652a7180fa5594c502649c1c9f7afdd7' ];then
-	echo "$0 test ok (status output)!"
-else
-	echo "$0 test failed (status output)!"
-	exit 1
-fi
+	if [ "${SSH_ID_TYPE}" == 'ecdsa' ];then
+		KC_ID_TYPE='ecdsa-sha2-nistp256'
+	else
+		KC_ID_TYPE="ssh-${SSH_ID_TYPE}"
+	fi
 
-${SSH_ADD} -d ${SSH_ID}
+	rm -f "${SSH_ID_FILE}" "${SSH_ID_FILE}".pub
+
+	${SSH_KEYGEN} -o -t "${SSH_ID_TYPE}" -C "${SSH_ID_COMMENT}" -N '' -f "${SSH_ID_FILE}"
+	if [ $? -ne 0 ];then
+		echo "$0 test failed (generate new ssh key)!"
+		exit 1
+	fi
+
+	${SSH_ADD} "${SSH_ID_FILE}"
+	if [ $? -ne 0 ];then
+		echo "$0 test failed (add new ssh key to agent)!"
+		exit 1
+	fi
+
+	rm -f "${KC_DB}"
+	if echo "write" |${KC_RUN} -A "${KC_ID_TYPE},${SSH_ID_COMMENT}" -b -k ${KC_DB};then
+		echo "$0 test ok (create new db with agent, key type ${SSH_ID_TYPE})!"
+	else
+		echo "$0 test failed (create new db with agent, key type ${SSH_ID_TYPE})!"
+		exit 1
+	fi
+
+	${SSH_ADD} -d ${SSH_ID_FILE}
+done
