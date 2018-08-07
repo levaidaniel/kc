a3629caeb75aa0f7066b542e899f50ca30b3fde5
LEVAI Daniel <leva@ecentrum.hu>
2018-08-07 12:50:53 +0200

    Extend commands' usage output with '-A' for SSH agent

--- commands_init.c
+++ commands_init.c
@@ -118,7 +118,7 @@ commands_init(command **commands)
 		(*commands) = (*commands)->next;
 
 		(*commands)->name = "import";
-		(*commands)->usage = "import -k <filename> [-P kdf] [-e cipher] [-m cipher_mode]";
+		(*commands)->usage = "import -k <filename> [-A <key type,key comment>] [-P kdf] [-e cipher] [-m cipher_mode]";
 		(*commands)->help = "Import and overwrite the current database with the one from a kc compatible encrypted database file named 'filename'. 'filename' must be a proper kc database. 'kdf' and 'cipher mode' can be used to specify these parameters if they differ from the current database.\nSee command 'importxml', 'export' and 'append'.";
 		(*commands)->fn = cmd_import;
 		(*commands)->next = (command *)malloc(sizeof(command)); malloc_check((*commands)->next);
@@ -159,7 +159,7 @@ commands_init(command **commands)
 		(*commands) = (*commands)->next;
 
 		(*commands)->name = "passwd";
-		(*commands)->usage = "passwd [-P <kdf>]";
+		(*commands)->usage = "passwd [-A <key type,key comment>] [-P <kdf>]";
 		(*commands)->help = "Change the database password and optionally the KDF. All changes will be written immediately.";
 		(*commands)->fn = cmd_passwd;
 		(*commands)->next = (command *)malloc(sizeof(command)); malloc_check((*commands)->next);
@@ -242,7 +242,7 @@ commands_init(command **commands)
 	(*commands) = (*commands)->next;
 
 	(*commands)->name = "export";
-	(*commands)->usage = "export -k <filename> [-P kdf] [-e cipher] [-m cipher_mode] [-c keychain]";
+	(*commands)->usage = "export -k <filename> [-A <key type,key comment>] [-P kdf] [-e cipher] [-m cipher_mode] [-c keychain]";
 	(*commands)->help = "Export the database to a kc compatible encrypted database file named 'filename' (if no extension specified, \".kcd\" will be appended). When specifying 'keychain', export only that keychain. 'keychain' can be the keychain's index number or name. Index number takes priority when addressing a keychain. 'kdf', 'cipher' and 'cipher_mode' can be used to specify a different KDF, encryption cipher and cipher mode to use while exporting the database.\nSee command 'dump', 'import' and 'append'.";
 	(*commands)->fn = cmd_export;
 	(*commands)->next = (command *)malloc(sizeof(command)); malloc_check((*commands)->next);
