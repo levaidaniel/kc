0b13df2b32250cd5e36b16e6910e2e783cc04ade
LEVAI Daniel <leva@ecentrum.hu>
2018-08-07 14:09:05 +0200

    regress/cmd_passwd.sh: test password change with SSH agent
    For this, do the ssha.sh test in one of the first steps (run_tests.sh)
    so the identities will be ready for the cmd_passwd.sh test.

--- regress/cmd_passwd.sh
+++ regress/cmd_passwd.sh
@@ -33,13 +33,27 @@ else
 	exit 1
 fi
 
-# Also fake an invalid parameter
-if printf "passwd --help\npasswd -P sha512\n${PASSWORD}\n${PASSWORD}\n" |${KC_RUN} -b -k ${KC_DB} -P bcrypt -p ${KC_PASSFILE};then
-	echo "$0 test ok (passwd bcrypt #2)!"
-else
-	echo "$0 test failed (passwd bcrypt #2)!"
+if [ -z "${SSH_AUTH_SOCK}" ];then
+	echo "$0 test failed (ssh-agent is not running)!"
 	exit 1
 fi
+
+ssh-add regress/test_id_rsa:2048_ssh
+if printf "passwd -A ssh-rsa,PuBkEy_+*()[]{}';,.!@#^&=-/|_comment-rsa:2048 -P bcrypt\n${PASSWORD}\n${PASSWORD}\n" |${KC_RUN} -b -k ${KC_DB} -P bcrypt -p ${KC_PASSFILE};then
+	echo "$0 test ok (SSH agent, passwd bcrypt #2)!"
+else
+	echo "$0 test failed (SSH agent, passwd bcrypt #2)!"
+	exit 1
+fi
+
+# Also fake an invalid parameter
+if printf "passwd --help\npasswd -P sha512\n${PASSWORD}\n${PASSWORD}\n" |${KC_RUN} -b -k ${KC_DB} -P bcrypt -A "ssh-rsa,PuBkEy_+*()[]{}';,.!@#^&=-/|_comment-rsa:2048";then
+	echo "$0 test ok (SSH agent, passwd bcrypt #3)!"
+else
+	echo "$0 test failed (SSH agent, passwd bcrypt #3)!"
+	exit 1
+fi
+ssh-add -d regress/test_id_rsa:2048_ssh
 
 if echo "" |${KC_RUN} -b -k ${KC_DB} -p ${KC_PASSFILE};then
 	echo "$0 test ok (reopen)!"
@@ -47,5 +61,6 @@ else
 	echo "$0 test failed (reopen)!"
 	exit 1
 fi
+
 
 exit 0
