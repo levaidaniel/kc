1c4f691fd24c18dfbd44c3c2db62a75e61b3a9ba
LEVAI Daniel <leva@ecentrum.hu>
2022-11-30 09:09:48 +0100

    bcrypt/blf.c: sync w/ upstream v1.9
    commit d073af4c15b6fcd85970fb9bf6858aceaefc64ee
    Author: jsg <jsg@openbsd.org>
    Date:   Sun Aug 28 11:11:25 2022 +0000
    
        remove unused blowfish inline defines
        inline use was removed in 1998

--- bcrypt/blf.c
+++ bcrypt/blf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: blf.c,v 1.8 2021/11/29 01:04:45 djm Exp $	*/
+/*	$OpenBSD: blf.c,v 1.9 2022/08/28 11:11:25 jsg Exp $	*/
 
 /*
  * Blowfish block cipher for OpenBSD
@@ -40,13 +40,6 @@
 #include <sys/param.h>
 
 #include "blf.h"
-
-#undef inline
-#ifdef __GNUC__
-#define inline __inline
-#else				/* !__GNUC__ */
-#define inline
-#endif				/* !__GNUC__ */
 
 /* Function for Feistel Networks */
 
